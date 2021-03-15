f946da296ccc438afaec0bbbe2078d1de7847cf7
Robson Barbosa Souza <powerrob92@gmail.com>
2021-03-15 13:52:13 -0300

    chore: config app struture

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
+    <>
+    <GlobalStyle/>
     <div className="App">
       <h1>Hello world!</h1>
     </div>
+    </>
   );
 }
