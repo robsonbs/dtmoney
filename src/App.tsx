f79494b1ba00aded88d9ababfecf422bb71501b6
Robson Barbosa Souza <powerrob92@gmail.com>
2021-03-15 17:15:57 -0300

    refact: modal new-transaction-modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
 
 Modal.setAppElement('#root');
@@ -19,14 +20,11 @@ export function App() {
   return (
     <>
       <GlobalStyle />
-      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <Modal
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-        >
-          <h2>Cadastrar transação</h2>
-        </Modal>
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal} />
     </>
   );
 }
