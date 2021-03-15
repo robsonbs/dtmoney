b1116faab3a237abae4c81c3deafe2e364f990d3
Robson Barbosa Souza <powerrob92@gmail.com>
2021-03-15 16:38:48 -0300

    chore: add MirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: "Desenvolvimento de Site",
+          type: "income",
+          amount: 12000,
+          category: "Development"
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
