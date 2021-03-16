d79d53bda37f43be6230bbbe51cb38e98049d908
Robson Barbosa Souza <powerrob92@gmail.com>
2021-03-15 22:16:54 -0300

    chore: mockup transactions

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,47 @@
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { App } from './App';
 
 createServer({
-  routes() {
-    this.namespace = 'api';
+  models: {
+    transaction: Model,
+  },
 
-    this.get('/transactions', () => {
-      return [
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
         {
           id: 1,
           title: 'Desenvolvimento de Site',
           type: 'income',
           amount: 12000,
-          category: 'Development',
+          category: 'Desenvolvimento',
+          createdAt: new Date(),
         },
-      ];
+        {
+          id: 2,
+          title: 'Luz',
+          type: 'outcome',
+          amount: 230.32,
+          category: 'Despesas',
+          createdAt: new Date(),
+        },
+      ],
+    });
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data);
     });
   },
 });
